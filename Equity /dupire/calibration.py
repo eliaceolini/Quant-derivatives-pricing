45fd82bff72ad4497323b22df7e659c428782098
Elia Ceolini <eliaceolini99@gmail.com>
2026-01-29 18:52:10 +0100

    feat: add Dupire calibration pipeline

--- Equity /dupire/calibration.py
+++ Equity /dupire/calibration.py
@@ -1,1 +1,21 @@
+from Equity.volatility_surface import VolatilitySurface
+from Equity.dupire.local_vol_surface import DupireLocalVolSurface
+
+
+class DupireCalibrator:
+    """
+    Calibration pipeline for Dupire local volatility model.
+    """
+
+    def __init__(self, market_data):
+        self.market_data = market_data
+
+    def calibrate(self):
+        """
+        Market implied vols -> smooth vol surface -> local vol surface
+        """
+        vol_surface = VolatilitySurface(self.market_data)
+        local_vol_surface = DupireLocalVolSurface(vol_surface)
+
+        return vol_surface, local_vol_surface
 
